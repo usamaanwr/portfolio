ff01a4ac8419650d1e2c6f2584606d8c47dbdb1c
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-05-05 11:53:16 +0500

    initialize App component with routing and navigation

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './componenst/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './componenst/About'
+
+
+
+function App() {
+ 
+  return (
+    
+    <BrowserRouter>
+    <Navbar /> 
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+    </BrowserRouter>
+
+   
+  )
+}
+
+export default App
