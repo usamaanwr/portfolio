c7774b26b925247f151d404f895a2b838ca05273
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-05-06 16:31:50 +0500

    feat: add project data for showcasing featured projects in the portfolio

---
+++ src/data/ProjectData.js
@@ -0,0 +1,26 @@
+const projectData = [
+    {
+      title: "Riwaya Store",
+      description: "A responsive e-commerce website built with HTML, CSS, and JavaScript. Features product categories, shopping cart, and contact form..",
+      tech: ["Html5", "css3", "javascript", "sweetalert2"],
+      image: "",
+      link: "https://riwa.netlify.app",
+      github: "https://github.com/usamaanwr/Riwaya-Store"
+    },
+    {
+      title: "Property Management Portal",
+      description: "A full-stack solution for managing properties, including user authentication and real-time updates.",
+      tech: ["Next.js", "Node.js", "Express", "MongoDB"],
+      link: "#",
+      github: "#"
+    },
+    {
+      title: "YouTube Backend System",
+      description: "A robust backend project involving user registration, secure login, and video history tracking.",
+      tech: ["Node.js", "Express", "MongoDB", "JWT"],
+      link: "#",
+      github: "#"
+    }
+  ];
+
+  export default projectData
