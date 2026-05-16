e94c3b02ad85a2bcfc2c8bfe44b0b4eece458374
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-05-16 17:02:04 +0500

    refactor: update project data structure and add new project details

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,26 +1,37 @@
 const projectData = [
-    {
-      title: "Riwaya Store",
-      description: "A responsive e-commerce website built with HTML, CSS, and JavaScript. Features product categories, shopping cart, and contact form..",
-      tech: ["Html5", "css3", "javascript", "sweetalert2"],
-      image: "",
-      link: "https://riwa.netlify.app",
-      github: "https://github.com/usamaanwr/Riwaya-Store"
-    },
-    {
-      title: "Property Management Portal",
-      description: "A full-stack solution for managing properties, including user authentication and real-time updates.",
-      tech: ["Next.js", "Node.js", "Express", "MongoDB"],
-      link: "https://property-management-system-lac.vercel.app/",
-      github: "https://github.com/usamaanwr/PropertyManagementSystem"
-    },
-    {
-      title: "YouTube Backend System",
-      description: "A robust backend project involving user registration, secure login, and video history tracking.",
-      tech: ["Node.js", "Express", "MongoDB", "JWT"],
-      link: "https://github.com/usamaanwr/Youtube-Backend-Project",
-      github: "https://github.com/usamaanwr/Youtube-Backend-Project"
-    }
-  ];
+  {
+    title: "Riwaya Store",
+    description:
+      "A responsive e-commerce website built with HTML, CSS, and JavaScript. Features product categories, shopping cart, and contact form..",
+    tech: ["Html5", "css3", "javascript", "sweetalert2"],
+    image: "",
+    link: "https://riwa.netlify.app",
+    github: "https://github.com/usamaanwr/Riwaya-Store",
+  },
+  {
+    title: "Authentication System",
+    description:
+      "A full-stack authentication system with JWT access & refresh tokens, HTTP-only cookies, protected routes, avatar upload via Cloudinary, and form validation on both frontend and backend.",
+    tech: [
+      "React.js",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "JWT",
+      "Cloudinary",
+      "DaisyUI",
+    ],
+    link: "https://authentication-a3fc.vercel.app",
+    github: "https://github.com/usamaanwr/authentication",
+  },
+  {
+    title: "YouTube Backend System",
+    description:
+      "A robust backend project involving user registration, secure login, and video history tracking.",
+    tech: ["Node.js", "Express", "MongoDB", "JWT"],
+    link: "https://github.com/usamaanwr/Youtube-Backend-Project",
+    github: "https://github.com/usamaanwr/Youtube-Backend-Project",
+  },
+];
 
-  export default projectData
+export default projectData;
